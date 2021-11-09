2f66de8f1a3df75c7d62ce84f2db094c74b6576b
Rafael Nogueira <rafanog.linux@hotmail.com>
2021-11-09 13:44:39 -0300

    atualização git ignore

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
+global using MinimalWebAPI.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
